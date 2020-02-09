fb3214b08afc28a3e7443671cd2cf46d5831d02e
Moshood temitope <temitopemoshood@gmail.com>
2020-02-09 18:27:24 +0100

    updated base url

--- banklite/src/services/urls.js
+++ banklite/src/services/urls.js
@@ -1,5 +1,6 @@
 
-const URL = 'http://52.170.209.118';
+// const URL = 'http://52.170.209.118';
+const URL = 'http://40.76.69.211/Banklot';
 
 const BASEURL = URL;  
 
@@ -8,79 +9,79 @@ export const routes = {
 
     // Administration endpoints
     
-    LOGIN_USER: BASEURL + '/Dars.Administration/api/Login',
-    HIT_DASHBOARD: BASEURL + '/Dars.Administration/api/Dashboard',
+    LOGIN_USER: BASEURL + '/api/Login',
+    HIT_DASHBOARD: BASEURL + '/api/Dashboard',
 
 
     // Disbursement endpoints
     
-    HIT_DISBURSEMENT: BASEURL + '/Dars.Administration/api/Disbursment',
+    HIT_DISBURSEMENT: BASEURL + '/api/Disbursment',
 
 
     // Administration endpoints
     
-    GET_BRANCHES: BASEURL + '/Dars.Administration/api/branch',
-    ADD_BRANCH: BASEURL + '/Dars.Administration/api/branch',
+    GET_BRANCHES: BASEURL + '/api/branch',
+    ADD_BRANCH: BASEURL + '/api/branch',
     
-    HIT_USERS: BASEURL + '/Dars.Administration/api/user',
+    HIT_USERS: BASEURL + '/api/user',
     
-    GET_ORGANIZATION_DETAILS: BASEURL + '/Dars.Administration/api/Administration/organization',
-    UPDATE_ORGANIZATION_DETAILS: BASEURL + '/Dars.Administration/api/Administration/updateorganization',
+    GET_ORGANIZATION_DETAILS: BASEURL + '/api/Administration/organization',
+    UPDATE_ORGANIZATION_DETAILS: BASEURL + '/api/Administration/updateorganization',
     
-    ADD_CURRENCY: BASEURL + '/Dars.Administration/api/Administration/currency',
-    UPDATE_CURRENCY: BASEURL + '/Dars.Administration/api/Administration/updatecurrency',
-    GET_ALL_CURRENCIES: BASEURL + '/Dars.Administration/api/Administration/currencies',
+    ADD_CURRENCY: BASEURL + '/api/Administration/currency',
+    UPDATE_CURRENCY: BASEURL + '/api/Administration/updatecurrency',
+    GET_ALL_CURRENCIES: BASEURL + '/api/Administration/currencies',
     
-    ADD_CUSTOMER_TYPE: BASEURL + '/Dars.Administration/api/CustomerType/customertype',
-    HIT_CUSTOMER_TYPES: BASEURL + '/Dars.Administration/api/CustomerType',
+    ADD_CUSTOMER_TYPE: BASEURL + '/api/CustomerType/customertype',
+    HIT_CUSTOMER_TYPES: BASEURL + '/api/CustomerType',
     
-    ADD_EXCHANGE_RATE: BASEURL + '/Dars.Administration/api/Account/exchangerate',
+    ADD_EXCHANGE_RATE: BASEURL + '/api/Account/exchangerate',
     
-    HIT_ROLE: BASEURL + '/Dars.Administration/api/Role', //works
+    HIT_ROLE: BASEURL + '/api/Role', //works
 
-    HIT_ACTIVITIES: BASEURL + '/Dars.Administration/api/Activities', //works
-    // ADD_TRANSACTION_CHANNEL: BASEURL + '/Dars.Administration/api/Account/transactionchannel',
+    HIT_ACTIVITIES: BASEURL + '/api/Activities', //works
+    // ADD_TRANSACTION_CHANNEL: BASEURL + '/api/Account/transactionchannel',
     
-    ADD_TRANSACTION_CHANNEL: BASEURL + '/Dars.Administration/api/TransactionChannel/transactionchannel',
-    HIT_TRANSACTION_CHANNEL: BASEURL + '/Dars.Administration/api/TransactionChannel/transactionchannels',
+    ADD_TRANSACTION_CHANNEL: BASEURL + '/api/TransactionChannel/transactionchannel',
+    HIT_TRANSACTION_CHANNEL: BASEURL + '/api/TransactionChannel/transactionchannels',
    
-    GET_ACCESS_PREFERENCE: BASEURL + '/Dars.Administration/api/Administration/accesspreference',
-    UPDATE_ACCESS_PREFERENCE: BASEURL + '/Dars.Administration/api/Administration/accesspreference',
+    GET_ACCESS_PREFERENCE: BASEURL + '/api/Administration/accesspreference',
+    UPDATE_ACCESS_PREFERENCE: BASEURL + '/api/Administration/accesspreference',
     
-    UPDATE_EMAIL_SETTINGS: BASEURL + '/Dars.Administration/api/Communication/emailsetting',
-    GET_EMAIL_SETTINGS: BASEURL + '/Dars.Administration/api/Communication/emailsetting/',
-    HIT_INTERNAL_CONTROL: BASEURL + '/Dars.Administration/api/internal',
-    GET_SMS_SETTINGS: BASEURL + '/Dars.Administration/api/Communication/smssetting/',
-    UPDATE_SMS_SETTINGS: BASEURL + '/Dars.Administration/api/Communication/smssetting',
+    UPDATE_EMAIL_SETTINGS: BASEURL + '/api/Communication/emailsetting',
+    GET_EMAIL_SETTINGS: BASEURL + '/api/Communication/emailsetting/',
+    HIT_INTERNAL_CONTROL: BASEURL + '/api/internal',
+    GET_SMS_SETTINGS: BASEURL + '/api/Communication/smssetting/',
+    UPDATE_SMS_SETTINGS: BASEURL + '/api/Communication/smssetting',
     
-    JOURNAL_ENTRIES: BASEURL + '/Dars.Administration/api/JournalEntries',
+    JOURNAL_ENTRIES: BASEURL + '/api/JournalEntries',
 
     //Accounting Endpoints
-    HIT_GLACCOUNTS: BASEURL + '/Dars.Administration/api/GLAccount/glaccounts',
-    ALL_GLACCOUNTS: BASEURL + '/Dars.Administration/api/GLAccount/allglaccounts',
-    CREATE_GLACCOUNT: BASEURL + '/Dars.Administration/api/GLAccount/glaccount',
+    HIT_GLACCOUNTS: BASEURL + '/api/GLAccount/glaccounts',
+    ALL_GLACCOUNTS: BASEURL + '/api/GLAccount/allglaccounts',
+    CREATE_GLACCOUNT: BASEURL + '/api/GLAccount/glaccount',
 
     //Deposits Endpoints
-    HIT_DEPOSITS: BASEURL + '/Dars.Administration/api/Deposits',
+    HIT_DEPOSITS: BASEURL + '/api/Deposits',
 
     //Deposits Transactions Endpoints
-    HIT_DEPOSITS_TRANSACTIONS: BASEURL + '/Dars.Administration/api/DepositTransactions',
+    HIT_DEPOSITS_TRANSACTIONS: BASEURL + '/api/DepositTransactions',
 
     //LOAN Endpoints
-    HIT_LOAN: BASEURL + '/Dars.Administration/api/Loans',
+    HIT_LOAN: BASEURL + '/api/Loans',
 
     //Loan Transactions Endpoints
-    HIT_LOAN_TRANSACTIONS: BASEURL + '/Dars.Administration/api/LoanTransactions',
+    HIT_LOAN_TRANSACTIONS: BASEURL + '/api/LoanTransactions',
 
     //Notification Endpoints
-    HIT_NOTIFICATIONS: BASEURL + '/Dars.Administration/api/Notifications',
+    HIT_NOTIFICATIONS: BASEURL + '/api/Notifications',
 
     //Clients Endpoints
-    HIT_CLIENTS: BASEURL + '/Dars.Administration/api/Clients',
+    HIT_CLIENTS: BASEURL + '/api/Clients',
 
     //Upload Data Endpoints
-    HIT_UPLOAD_DATA: BASEURL + '/Dars.Administration/api/Upload',
+    HIT_UPLOAD_DATA: BASEURL + '/api/Upload',
 
     //Loan Product Endpoints
-    HIT_LOAN_PRODUCTS: BASEURL + '/Dars.Administration/api/LoanProducts',
+    HIT_LOAN_PRODUCTS: BASEURL + '/api/LoanProducts',
 }
